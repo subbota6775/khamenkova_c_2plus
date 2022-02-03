a8acda5ca89c783086bb84a745f123795f4ab7af
subbota6775 <98962637+subbota6775@users.noreply.github.com>
2022-02-03 19:40:54 +0300

    Add files via upload

---
+++ TASKS/Task1/Source1.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+    int i, a[100], n;
+    cout << "Enter dimension of array --> ";
+    cin >> n;
+    for (i = 0;i < n;i++)
+    {
+        cout << "Введи Элемент номер " << i + 1 << " --> ";
+        cin >> a[i];
+    }
+    cout << "\n\nElements kratnye 3 --> ";
+    int count = 0;
+    for (i = 0;i < n;i++)
+    {
+        if (a[i] % 3 == 0)
+        {
+            cout << a[i] << " ";
+            count++;
+        }
+    }
+    cout << "\nCount of this elements = " << count;
+}
