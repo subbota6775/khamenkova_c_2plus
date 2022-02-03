33e5aba32c54b6054c0f02acfa14a9b9cee5ec29
subbota6775 <98962637+subbota6775@users.noreply.github.com>
2022-02-03 19:14:11 +0300

    Add files via upload

---
+++ TASKS/Task1/Hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int a;
+    cout << "Введите число" << endl;
+    cin >> a;
+
+    if (a > 7)
+    {
+        cout << "Привет!" << endl;
+    }
+
+
+}
